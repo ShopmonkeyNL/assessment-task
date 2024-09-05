9bc2b06d530d15e33698799700f3f25b98739be8
Shopmonkey <jesseruben.vanloon@gmail.com>
2024-09-05 15:00:39 +0200

    first version

---
+++ global.js
@@ -0,0 +1,13 @@
+// SCHRIJF HIER DE FUNCTIE VOOR HET INLADEN VAN DE TWEEDE PRODUCTAFBEELDING
+// ONDERSTAAND DE JSON URLS:
+// https://www.comfortclubclothing.com/products/scarab-tee-off-white.json
+// https://www.comfortclubclothing.com/products/papyrus-tee-off-white.json
+// https://www.comfortclubclothing.com/products/wedjat-tee-taupe.json
+
+$(document).ready(function () {
+  console.log("ready!");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hello World!");
+});
